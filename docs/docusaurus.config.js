40a31bc4c4277fe2842d16758db3fcc570939f88
Eva <eva@Evas-Mac-mini.local>
2026-02-25 12:11:40 +0200

    Warn on broken links instead of failing

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -30,7 +30,8 @@ const config = {
   projectName: 'aloro-dashboard', // Repo name
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
